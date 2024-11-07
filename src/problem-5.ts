92c790d004c2eb7fda263fac755dd3b9a14d51e2
arifanadia <contactarifanadia@gmail.com>
2024-11-07 18:07:55 +0600

    problem5  solved

---
+++ src/problem-5.ts
@@ -0,0 +1,12 @@
+{
+  const person = {
+    name: "Alice",
+    age: 30,
+  };
+
+  function getProperty<X, Y extends keyof X>(obj: X, key: Y) {
+    return obj[key];
+  }
+
+  console.log(getProperty(person, "name"));
+}
