0025189e16bd1e1643eab9edb0710486cae2c5c2
arifanadia <contactarifanadia@gmail.com>
2024-11-07 19:23:11 +0600

    problem7  solved

---
+++ src/problem-7.ts
@@ -0,0 +1,17 @@
+{
+  class Car {
+    constructor(
+      public make: string,
+      public model: string,
+      public year: number
+    ) {}
+    getCarAge() {
+      const currentYear = new Date().getFullYear();
+      return currentYear - this.year;
+    }
+  }
+  const car = new Car("Honda", "Civic", 2018);
+  car.getCarAge();
+//   console.log(car.getCarAge());
+}
+
